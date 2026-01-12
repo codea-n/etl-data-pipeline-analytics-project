705675decea33169041dfd70001eb0ecb61434c3
codea-n <ali.hamza12714@gmail.com>
2026-01-11 20:13:39 -0600

    added function create_table into etl.py

--- db.py
+++ db.py
@@ -7,3 +7,25 @@ def get_connection():
     """
     return sqlite3.connect(DB_FILE)
 
+def create_table():
+    """
+    Create a table if it does not exist
+    """
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS country_borders;")
+    
+    sql = """
+    CREATE TABLE IF NOT EXISTS country_borders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        country_code TEXT,
+        country_name TEXT,
+        border_country_code TEXT,
+        CONSTRAINT unique_borders UNIQUE(country_code,border_country_code)
+    );
+    """
+    
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
